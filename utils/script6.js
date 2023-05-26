241de9717a1fba8ab92feaaec4442d460a9dbfd9
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2023-05-26 12:15:32 +0530

    Update script6.js

--- utils/script6.js
+++ utils/script6.js
@@ -19,3 +19,20 @@ x = Number.MIN_VALUE;              //Give min value of Number type
 
 
 console.log(x, typeof x);
+
+y = Math.sqrt(64);                //Give square root of number
+y = Math.abs(-4);                 //Give absolute value of number
+y = Math.pow(2, 3);               //Give power of number
+y = Math.PI;                      //Give value of PI
+y = Math.min(2, 3, 4, 5, 6, 7);   //Give minimum value of number
+y = Math.max(2, 3, 4, 5, 6, 7);   //Give maximum value of number
+
+y = Math.round(234.6);            //Round off number
+y = Math.ceil(34.3);              //Round off number to upper value
+y = Math.floor(235.7);            //Round off number to lower value
+y = Math.random();                //Give random number between 0 and 1 (not including 1) with decimal
+y = Math.floor(Math.random() * 10 + 1);  //Give random number between 1 and 10 (including 10) without decimal
+
+
+console.log(y, typeof y);
+console.log((9+8).toString(),(9).toString());
